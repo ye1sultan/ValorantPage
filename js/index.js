837a71ff875d65b835b337935c50a64e9753771d
Yels <example@gmail.com>
2021-07-21 22:48:59 +0600

    Created pages for guns and agents. Also created page for maps, but have to finish it. Just added map images

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-(function() {
+(function () {
     document.addEventListener('DOMContentLoaded', () => {
         let typeWriter = document.querySelector('.typing');
 
-        let str = 'Welcome to <span class="valorant">valorant</span> data ',
+        let str = 'Welcome to <span class="valorant">valorant</span> <span class="last-word">data</span> ',
             i = 0,
             isTag,
             text;
+
+        str += " ";
 
         function type() {
             text = str.slice(0, ++i);
@@ -30,62 +32,38 @@
                 return type();
             }
 
-            if(i === 54) {
+            if (i === str.length - 2) {
                 setTimeout(() => {
                     document.querySelector('.title').style.animation = 'blink 1s step-end infinite';
                 }, 500);
+                return;
             }
 
             setTimeout(type, 75);
         }
 
+        function setSlider() {
+            const sliderProps = {
+                type: 'loop',
+                autoplay: true,
+                direction: 'ttb',
+                height: '500px',
+                perPage: 3,
+                perMove: 1,
+                speed: 1000,
+                interval: 2500,
+                updateOnMove: true,
+                focus: 'center',
+                arrows: false,
+                pagination: false
+            }
+
+            new Splide('.agent-slider', sliderProps).mount();
+            new Splide('.map-slider', sliderProps).mount();
+            new Splide('.weapon-slider', sliderProps).mount();
+        }
+
         type();
-
-        // Slider
-        new Splide( '.agent-slider', {
-            autoplay: true,
-            type: 'loop',
-            perPage: 3,
-            perMove: 1,
-            direction: 'ttb',
-            height: '500px',
-            arrows: false,
-            pagination: false,
-            speed: 1000,
-            interval: 2500,
-            updateOnMove: true,
-            focus: 'center'
-        }).mount();
-
-        new Splide( '.map-slider', {
-            autoplay: true,
-            type: 'loop',
-            perPage: 3,
-            perMove: 1,
-            direction: 'ttb',
-            height: '500px',
-            arrows: false,
-            pagination: false,
-            speed: 1000,
-            interval: 2500,
-            updateOnMove: true,
-            focus: 'center'
-        }).mount();
-
-        new Splide( '.weapon-slider', {
-            autoplay: true,
-            type: 'loop',
-            perPage: 3,
-            perMove: 1,
-            direction: 'ttb',
-            height: '500px',
-            arrows: false,
-            pagination: false,
-            speed: 1000,
-            interval: 2500,
-            updateOnMove: true,
-            focus: 'center'
-        }).mount();
-        // Slider
+        setSlider();
     })
 }());
