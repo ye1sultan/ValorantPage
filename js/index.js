a93af4f7d913a52c6733ef0ec5efb8929f658fcf
Yels <example@gmail.com>
2021-07-21 17:52:42 +0600

    Added carousels

--- js/index.js
+++ js/index.js
@@ -1,28 +1,91 @@
-let typeWriter = document.querySelector('.typing');
-let str = 'Welcome to <span class="valorant">valorant</span> data ',
-    i = 0,
-    isTag,
-    text;
+(function() {
+    document.addEventListener('DOMContentLoaded', () => {
+        let typeWriter = document.querySelector('.typing');
 
-(function type() {
-    text = str.slice(0, ++i);
-    if (text === str) return;
-    
-    typeWriter.innerHTML = text;
+        let str = 'Welcome to <span class="valorant">valorant</span> data ',
+            i = 0,
+            isTag,
+            text;
 
-    var char = text.slice(-1);
+        function type() {
+            text = str.slice(0, ++i);
 
-    if( char === '<' ) isTag = true;
-    if( char === '>' ) isTag = false;
+            if (text === str) {
+                return;
+            }
 
-    if (isTag) return type();
-    setTimeout(type, 75);
+            typeWriter.innerHTML = text;
+
+            let char = text.slice(-1);
+
+            if (char === '<') {
+                isTag = true;
+            }
+
+            if (char === '>') {
+                isTag = false;
+            }
+
+            if (isTag) {
+                return type();
+            }
+
+            if(i === 54) {
+                setTimeout(() => {
+                    document.querySelector('.title').style.animation = 'blink 1s step-end infinite';
+                }, 500);
+            }
+
+            setTimeout(type, 75);
+        }
+
+        type();
+
+        // Slider
+        new Splide( '.agent-slider', {
+            autoplay: true,
+            type: 'loop',
+            perPage: 3,
+            perMove: 1,
+            direction: 'ttb',
+            height: '500px',
+            arrows: false,
+            pagination: false,
+            speed: 1000,
+            interval: 2500,
+            updateOnMove: true,
+            focus: 'center'
+        }).mount();
+
+        new Splide( '.map-slider', {
+            autoplay: true,
+            type: 'loop',
+            perPage: 3,
+            perMove: 1,
+            direction: 'ttb',
+            height: '500px',
+            arrows: false,
+            pagination: false,
+            speed: 1000,
+            interval: 2500,
+            updateOnMove: true,
+            focus: 'center'
+        }).mount();
+
+        new Splide( '.weapon-slider', {
+            autoplay: true,
+            type: 'loop',
+            perPage: 3,
+            perMove: 1,
+            direction: 'ttb',
+            height: '500px',
+            arrows: false,
+            pagination: false,
+            speed: 1000,
+            interval: 2500,
+            updateOnMove: true,
+            focus: 'center'
+        }).mount();
+        // Slider
+    })
 }());
-
-
-
-
-
-
-
-
